5ee6c543a2e47c1c3dd50db7a93f018dd20bb6ab
David Cramer <dcramer@gmail.com>
2020-04-20 17:23:45 -0700

    Enable transactions/aiohttp for appdaemon

--- roles/hive.hass/files/apps/hive/sentry.py
+++ roles/hive.hass/files/apps/hive/sentry.py
@@ -1,6 +1,7 @@
 import logging
 
 import sentry_sdk
+from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
 
 import appdaemon.utils as utils
@@ -9,15 +10,18 @@ import hassapi as hass
 
 class Sentry(hass.Hass):
     def initialize(self):
-        # https://docs.sentry.io/platforms/python/logging/
-        sentry_logging = LoggingIntegration(
-            level=logging.INFO,  # Capture info and above as breadcrumbs
-            event_level=logging.ERROR,  # Send errors as events
-        )
-
         sentry_sdk.init(
             dsn=self.args.get("dsn"),
             environment=self.args.get("environment"),
-            integrations=[sentry_logging],
+            in_app_include=["hive"],
+            integrations=[
+                AioHttpIntegration(),
+                LoggingIntegration(
+                    level=logging.INFO,  # Capture info and above as breadcrumbs
+                    event_level=logging.ERROR,  # Send errors as events
+                ),
+            ],
             release=self.args.get("release") or f"appdaemon-{utils.__version__}",
+            traces_sample_rate=1.0,
+            traceparent_v2=True,
         )
